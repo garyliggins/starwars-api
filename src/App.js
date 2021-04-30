bc5f086ad3c49db75255e9cee8386c444ea01e11
gary <gary@ligginsdevelopment.com>
2021-04-30 16:41:57 -0700

    playing with star wars api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
+import React, {useState} from 'react'
 import './App.css';
 
 function App() {
+
+  const [char,setChar] =useState({})
+  const [count,setCount] =useState(1)
+  const [charList, setCharList] =useState([])
+
+  const getChar = () => {
+    console.log("making a request to api")
+    fetch(`https://swapi.dev/api/people/${count}/`)
+    .then((res) => res.json())
+    // .then(res => console.log(res.name))
+    .then((res) => {
+      setChar(res)
+      console.log(res)
+      setCharList([...charList, res])
+      setCount( count + 1)
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+    
+  }
+
   return (
+      
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Star Wars API</h1>
+      <button onClick={getChar}>Click to request a Character</button>
+      <h2> Current character</h2>
+      <p>Name: {char.name}</p>
+      <p>Height: {char.height}</p>
+      <p>Gender: {char.gender}</p>
+
+      <div>
+        <h2> Previously requested characters</h2>
+
+        <ul>
+          {
+            charList.map((characters, i) => 
+            <li key={i}>{characters.name}</li>
+            )
+            }
+        </ul>
+      </div>
+
     </div>
   );
 }
